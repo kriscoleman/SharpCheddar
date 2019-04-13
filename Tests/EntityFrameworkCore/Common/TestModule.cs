d7f8c0cbc089b3bc8fcaad641ee1236b9318ec7a
kcoleman <kcoleman@springthrough.com>
2019-04-13 01:02:34 -0400

    made changes to introduce autofac

---
+++ Tests/EntityFrameworkCore/Common/TestModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using SharpCheddar.Core;
+using SharpCheddar.EntityFrameworkCore;
+
+namespace Tests.EntityFrameworkCore.Common
+{
+    class TestModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            // for ef, you register your db context, which will get injected into your ef repos
+            builder.RegisterType<TestDbContext>().As<DbContext>();
+
+            // now register your repo
+            builder.RegisterType<EntityFrameworkCoreRepository<EntityFrameworkCoreTests.MyModel, int>>()
+                .As<IRepository<EntityFrameworkCoreTests.MyModel, int>>();
+        }
+    }
+}
